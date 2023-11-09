c515eca01716ce98439d2a8c234223e4ee3dcbcb
Clim Jark <climjark@protonmail.com>
2023-11-08 20:00:18 -0800

    Added allocation for userFunctions
    Also commented the code for netEventHandler

--- include/net/netEventHandler.h
+++ include/net/netEventHandler.h
@@ -1,5 +1,5 @@
 #ifndef FOUNDATIONS_NET_NETEVENTHANDLER_H_
-#define FOUNDATIONS_NET_NETEVENTHANDLER_H_
+#define FOUNDATIONS_NET_NETEVENTHANDLER_H_ 1
 
 // Standard header for size_t
 #ifndef STDDEF_DEFINED
@@ -13,16 +13,24 @@
 #define STDINT_DEFINED 1
 #endif
 
+// Define the user function structure as 'userFunction_t'
 typedef struct userFunction userFunction_t;
+// Define the net event handler structure as 'netEventHandler_t'
 typedef struct netEventHandler netEventHandler_t;
 
-uint16_t userFunction_getID(userFunction_t * function);
-void * userFunction_getFunction(userFunction_t * function);
-void * userFunction_getUserData(userFunction_t * function);
+// Get the identifier of a user function
+uint16_t userFunction_getID(userFunction_t *function);
 
-void register_user_function(user_function_t *function);
+// Get the function pointer of a user function
+void *userFunction_getFunction(userFunction_t *function);
+
+// Get the user data associated with a user function
+void *userFunction_getUserData(userFunction_t *function);
+
+// Register a user function in the event handler
+void register_user_function(userFunction_t *function);
+
+// Process an event with the specified event ID
 void process_event(uint16_t event_id);
 
-
-
-#endif
+#endif // FOUNDATIONS_NET_NETEVENTHANDLER_H_
